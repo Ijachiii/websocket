902013aa90055488d0dd6a64fcd626603a1c646b
David Audu <davidaudu1010@gmail.com>
2023-11-02 00:09:28 +0100

    redis setup

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -92,10 +92,19 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [os.getenv("REDIS_URL")],
         },
     },
 }
+
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [("127.0.0.1", 6379)],
+#         },
+#     },
+# }
 
 
 # Database
